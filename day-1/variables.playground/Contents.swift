6259012ac92940c2f1d7d86720033fa880c0e67e
Igor Rodrigues <igorsteixeira94@gmail.com>
2022-07-31 18:04:01 -0300

    day-1

--- day-1/variables.playground/Contents.swift
+++ day-1/variables.playground/Contents.swift
@@ -1,3 +1,31 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+// let - make constant (apple recommend use)
+// var - make variable
+
+// type explicity
+let age: Int = 28
+
+// type implicitly
+let width = 94
+
+// long int
+let year = 9_000_000_00
+
+// multi-line strings
+var str = """
+This is multi-line strings.
+"I'm ios developer"
+"""
+
+// string interpolate
+let name_and_age = "My name is Igor Rodrigues and I am \(age) years old."
+
+//Doubles and decimal
+
+let pi = 3.141190283032
+let decimal_pi: Decimal = 3.141190283032
+
+// Bool
+
+let isTrue = false
